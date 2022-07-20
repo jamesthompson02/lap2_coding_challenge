2d8e521b8a53a51af75ff723e13e55ef16734378
sam <sampurna.chapagainnn@gmail.com>
2022-07-20 09:53:19 +0100

    add index.js and server.js files to lead the server and basic route setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const postRoutes = require('./controllers/posts');
+
+server.use('/dogs', postRoutes);
+
+const port = process.env.PORT || 3000;
+
+// Root route
+server.get('/', (req, res) => res.send('Hello, world!'));
+
+module.exports = server;
