ba47b3b4b6a4bf293a1abda013042520dbbd511e
jamesthompson02 <jamesthompsongithub@outlook.com>
2022-07-20 16:47:47 +0100

    Made a whole load of changes

--- server/dbConfig/devSeeds.sql
+++ server/dbConfig/devSeeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO posts (title, pseudonym, body, date)
+INSERT INTO posts (title, pseudonym, body, date1)
 VALUES
     ('test title one', 'Anonymous', 'this is a test title', '2022-07-20'),
     ('test title one', 'Blogger', 'this is another test title', '2022-06-20');
